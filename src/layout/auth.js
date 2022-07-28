dc53adb4f15d137e87f57d3e3fbaa0493fe0ad87
vengatbap <vengatbap.7@gmail.com>
2022-07-29 01:35:39 +0530

    auth

--- src/layout/auth.js
+++ src/layout/auth.js
@@ -1,18 +1,16 @@
 
-import { Link } from "react-router-dom";
 import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Sidenav from '../component/sidenav'
-import TopHaeder from "../component/header";
+
 function Authlayout(props) {   
     return (
         <>
 
         <Container>
           <Row className="align-items-center">
-            <Col md={3}></Col><Col md={6}> {props.children}</Col><Col md={3}></Col>
+            <Col lg={3}></Col><Col lg={6}> {props.children}</Col><Col lg={3}></Col>
           </Row>
         </Container>
         
